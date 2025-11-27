940a7825de25bb0fb1c675db247093151010b77d
Ilham Nasution <ilham.dev20@gmail.com>
2025-11-27 10:40:31 +0700

    initialize tanstack start

--- app/entry-server.tsx
+++
@@ -1,7 +0,0 @@
-import { StartServer, createStartHandler } from '@tanstack/start/server';
-import { createRouter } from './router';
-
-export default createStartHandler({
-  createRouter,
-  render: ({ router }) => <StartServer router={router} />,
-});
